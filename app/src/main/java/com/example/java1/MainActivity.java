e6378ca2f37d62eea8ba8320dda8bf7e78c65c32
Sharkz Reigns <87173929+SHARKZTECH@users.noreply.github.com>
2022-08-30 14:55:50 +0300

    1

---
+++ app/src/main/java/com/example/java1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.java1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void disable(View v){
+        v.setEnabled(false);
+        Button b=(Button) v;
+        b.setText("Disabled");
+
+        View txt=findViewById(R.id.text);
+        TextView textView=(TextView) txt;
+        textView.setText("Sharkz Reigns");
+
+    }
+}
